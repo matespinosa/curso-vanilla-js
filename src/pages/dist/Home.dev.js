ea05d0615e6d50b1ab1df12d1581ae058d1f4c75
Mateo Espinosa <matespinosa09@gmail.com>
2020-08-22 18:03:24 -0500

    obtencion del getdata

--- src/pages/dist/Home.dev.js
+++ src/pages/dist/Home.dev.js
@@ -5,9 +5,32 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+var _getData = _interopRequireDefault(require("../utils/getData"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var Home = function Home() {
-  var view = "\n        <div class=\"Characters\">\n            <article class=\"Character-item\">\n                <a href=\"#/1/\">\n                    <img src=\"image\" alt=\"name\">\n                    <h2>Name</h2>\n                </a>\n            </article>\n        </div>\n    ";
-  return view;
+  var characters, view;
+  return regeneratorRuntime.async(function Home$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap((0, _getData["default"])());
+
+        case 2:
+          characters = _context.sent;
+          view = "\n        <div class=\"Characters\">\n            ".concat(characters.results.map(function (character) {
+            return "\n                <article class=\"Character-item\">\n                    <a href=\"#/".concat(character.id, "/\">\n                        <img src=\"").concat(character.image, "\" alt=\"").concat(character.name, "\">\n                        <h2>").concat(character.name, "</h2>\n                    </a>\n                    </article>\n            \n            ");
+          }).join(''), "\n\n        </div>\n    ");
+          return _context.abrupt("return", view);
+
+        case 5:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
 };
 
 var _default = Home;
